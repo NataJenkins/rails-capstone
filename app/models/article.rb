5df0f0d6a70e7e1ea7d5d2691cf6a844e37f2d6d
NataJenkins <38480860+NataJenkins@users.noreply.github.com>
2021-01-29 16:39:19 -0600

    Active storage

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,6 @@ class Article < ApplicationRecord
 
     has_many :article_categories, dependent: :destroy
     has_many :categories, through: :article_categories
-    
 
+    has_one_attached :avatar
 end
--- db/migrate/20210129173912_create_votes.rb
+++ db/migrate/20210129173912_create_votes.rb
@@ -1,7 +1,6 @@
 class CreateVotes < ActiveRecord::Migration[6.1]
   def change
     create_table :votes do |t|
-
       t.timestamps
     end
   end
