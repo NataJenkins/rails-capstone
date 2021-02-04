119bbd919e06a83dca3ab30d0c532fa829f9474b
NataJenkins <38480860+NataJenkins@users.noreply.github.com>
2021-02-04 17:16:43 -0600

    models tests

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+    describe 'Check the presence of columns' do
+        it { should validate_presence_of(:name) }
+        it { should validate_presence_of(:priority) }
+      end
+      
+  describe 'Name should be unique' do
+    it { should validate_uniqueness_of(:name) }
+  end
+
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe User, type: :model do
       describe 'Username uniqueness' do
         it { should validate_uniqueness_of(:username) }
       end  
+
+      describe 'username and name should be filled' do
+        it { should validate_presence_of(:username) }
+        it { should validate_presence_of(:name) }
+      end
+
+      describe 'association' do
+        it { should have_many(:articles) }
+        it { should have_many(:votes) }
+      end
 end
