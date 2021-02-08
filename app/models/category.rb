7394e0cbd2b7780d10629e7e7f4f75648069fae1
NataJenkins <38480860+NataJenkins@users.noreply.github.com>
2021-02-08 15:07:53 -0600

    linters fix

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,7 @@ class Category < ApplicationRecord
   validates :name, uniqueness: true
 
   has_many :article_categories, dependent: :destroy
-  has_many :articles, -> {includes(:avatar_attachment)}, through: :article_categories, dependent: :destroy
+  has_many :articles, -> { includes(:avatar_attachment) }, through: :article_categories, dependent: :destroy
 
   def first_article
     articles.includes(:avatar_attachment).order(created_at: :desc).first
